9e644fe08033d347a340f6a6ac59d2eb67ae3362
MatheusZanon <mzblannes@outlook.com>
2024-05-24 13:59:43 -0300

    refactor: adicionando botoes para preview e upload de arquivos no drive

--- backend/human_app/views/gdrive_views.py
+++ backend/human_app/views/gdrive_views.py
@@ -1,11 +1,14 @@
 import os
+import io
 from dotenv import load_dotenv
 from operator import itemgetter
 from human_app.services.google_drive_service import Create_Service
+from django.http import HttpResponse, FileResponse
 from rest_framework import status, viewsets
 from rest_framework.views import Response
 from rest_framework.decorators import action, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from googleapiclient.http import MediaIoBaseDownload
 
 
 #------------------- CONFIGURACOES GOOGLE DRIVE -------------------
@@ -41,13 +44,35 @@ class GoogleDriveViewSet(viewsets.ModelViewSet):
             print(error)
             return Response(f"{error}", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
-    @action(detail=False, methods=['get'], url_path='preview_arquivo')
-    def preview_arquivo(self, request, ):
+    @staticmethod
+    def download_arquivo(service, arquivo_id):
+        request = service.files().get_media(fileId=arquivo_id)
+        file_io = io.BytesIO()
+        downloader = MediaIoBaseDownload(file_io, request)
+        
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+        
+        file_io.seek(0)
+        return file_io
+
+    @action(detail=False, methods=['get'], url_path='serve_file_preview')
+    def serve_file_preview(self, request):
         try:
+            print("serve_file_preview teste")
             arquivo_id = request.query_params.get('arquivo_id')
             service = Create_Service(SECRET_SERVICE_FILE, API_NAME, API_VERSION, SCOPES)
-            response = service.files().get(fileId=arquivo_id).execute()
-            return Response(response, status=status.HTTP_200_OK)
+            request = service.files().get_media(fileId=arquivo_id)
+            file_io = io.BytesIO()
+            downloader = MediaIoBaseDownload(file_io, request)
+
+            done = False
+            while not done:
+                _, done = downloader.next_chunk()
+            file_io.seek(0)
+
+            return FileResponse(file_io, as_attachment=False, filename='downloaded_file.pdf', content_type='application/pdf')
         except Exception as error:
             print(error)
             return Response(f"{error}", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
