34c6a1028dbf2fdd6c2d8380952d82957fe7a4a0
MatheusZanon <mzblannes@outlook.com>
2024-05-31 10:45:49 -0300

    refactor: mudando o modal para renderizar o arquivo uma vez a cada preview

--- backend/human_app/views/gdrive_views.py
+++ backend/human_app/views/gdrive_views.py
@@ -9,6 +9,8 @@ from rest_framework.views import Response
 from rest_framework.decorators import action, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from googleapiclient.http import MediaIoBaseDownload
+from django.views.decorators.clickjacking import xframe_options_exempt, xframe_options_deny, xframe_options_sameorigin
+
 
 
 #------------------- CONFIGURACOES GOOGLE DRIVE -------------------
@@ -57,12 +59,18 @@ class GoogleDriveViewSet(viewsets.ModelViewSet):
         file_io.seek(0)
         return file_io
 
+    @xframe_options_exempt
     @action(detail=False, methods=['get'], url_path='serve_file_preview')
     def serve_file_preview(self, request):
         try:
-            print("serve_file_preview teste")
             arquivo_id = request.query_params.get('arquivo_id')
+            print("Arquivo ID:", arquivo_id)
             service = Create_Service(SECRET_SERVICE_FILE, API_NAME, API_VERSION, SCOPES)
+            
+            file_info = service.files().get(fileId=arquivo_id, fields='name, mimeType').execute()
+            mimeType = file_info.get('mimeType')
+            filename = file_info.get('name')
+
             request = service.files().get_media(fileId=arquivo_id)
             file_io = io.BytesIO()
             downloader = MediaIoBaseDownload(file_io, request)
@@ -72,7 +80,7 @@ class GoogleDriveViewSet(viewsets.ModelViewSet):
                 _, done = downloader.next_chunk()
             file_io.seek(0)
 
-            return FileResponse(file_io, as_attachment=False, filename='downloaded_file.pdf', content_type='application/pdf')
+            return FileResponse(file_io, as_attachment=False, filename=filename, content_type=mimeType)
         except Exception as error:
             print(error)
             return Response(f"{error}", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
