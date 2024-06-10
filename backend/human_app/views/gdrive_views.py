64c3ef2f657014476bd30f7b7a5fe9c58964d797
MatheusZanon <mzblannes@outlook.com>
2024-06-10 17:08:10 -0300

    feat: upload de arquivos no drive concluido

--- backend/human_app/views/gdrive_views.py
+++ backend/human_app/views/gdrive_views.py
@@ -1,5 +1,6 @@
 import os
 import io
+import tempfile
 from dotenv import load_dotenv
 from operator import itemgetter
 from human_app.services.google_drive_service import Create_Service
@@ -8,7 +9,7 @@ from rest_framework import status, viewsets
 from rest_framework.views import Response
 from rest_framework.decorators import action, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from googleapiclient.http import MediaIoBaseDownload
+from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
 from django.views.decorators.clickjacking import xframe_options_exempt, xframe_options_deny, xframe_options_sameorigin
 
 
@@ -45,19 +46,53 @@ class GoogleDriveViewSet(viewsets.ModelViewSet):
         except Exception as error:
             print(error)
             return Response(f"{error}", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+    @action(detail=False, methods=['post'], url_path='upload_arquivo')
+    def upload_arquivo(self, request):
+        try:
+            files = request.FILES.getlist('files')
+            mime_types = request.data.getlist('mime_types')
+            folder_id = request.data.get('parents')
+            service = Create_Service(SECRET_SERVICE_FILE, API_NAME, API_VERSION, SCOPES)
+
+            for file, mime_type in zip(files, mime_types):
+                file_metadata = {
+                    'name': file.name,
+                    'parents': [folder_id],
+                }
+                print(file_metadata)
+
+                # Cria um arquivo temporário se necessário
+                if hasattr(file, 'temporary_file_path'):
+                    file_path = file.temporary_file_path()
+                else:
+                    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                        temp_file.write(file.read())
+                        file_path = temp_file.name
+
+                media = MediaFileUpload(file_path, mimetype=mime_type)
+                service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+            return Response("Arquivos enviados com sucesso.", status=status.HTTP_200_OK)
+        except Exception as error:
+            print(error)
+            return Response(f"{error}", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
     @staticmethod
     def download_arquivo(service, arquivo_id):
-        request = service.files().get_media(fileId=arquivo_id)
-        file_io = io.BytesIO()
-        downloader = MediaIoBaseDownload(file_io, request)
-        
-        done = False
-        while done is False:
-            status, done = downloader.next_chunk()
-        
-        file_io.seek(0)
-        return file_io
+        try:
+            request = service.files().get_media(fileId=arquivo_id)
+            file_io = io.BytesIO()
+            downloader = MediaIoBaseDownload(file_io, request)
+            
+            done = False
+            while done is False:
+                status, done = downloader.next_chunk()
+            
+            file_io.seek(0)
+            return Response(file_io, status=status.HTTP_200_OK)
+        except Exception as error:
+            print(error)
+            return Response(f"{error}", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     @xframe_options_exempt
     @action(detail=False, methods=['get'], url_path='serve_file_preview')
