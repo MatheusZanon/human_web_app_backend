6e293062df26aa20ae4300283399ff7c44308d43
MatheusZanon <mzblannes@outlook.com>
2024-06-13 16:08:11 -0300

    feat: terminada adaptacao da plataforma para refazer o processo do robo organiza extrato

--- backend/human_app/views/gdrive_views.py
+++ backend/human_app/views/gdrive_views.py
@@ -104,23 +104,58 @@ class GoogleDriveViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=['post'], url_path='upload_extrato_robo')
     def upload_extrato_robo(self, request):
         try:
-            folder_id = request.query_params.get('folder_id')
+            ano = request.query_params.get('ano')
+            mes = request.query_params.get('mes')
+            if int(mes) < 10:
+                mes = f"0{mes}"
+            files = request.FILES.getlist('files')
+            mime_types = request.data.getlist('mime_types')
+            folder_id = request.data.get('parents')
+
             service = Create_Service(SECRET_SERVICE_FILE, API_NAME, API_VERSION, SCOPES)
             query = f"parents in '{folder_id}'"
 
             response = service.files().list(q=query, fields="nextPageToken, files(id, name, mimeType)").execute()
             arquivos = response.get('files', [])
             for arquivo in arquivos:
-                if (arquivo['mimeType'] == 'application/vnd.google-apps.folder'
-                and arquivo['name'].__contains__(f"faturas_human_{datetime.now().strftime('%Y')}_{datetime.now().strftime('%m')}")):
-                    folder_metadata = {
-                        'name': 'novos_extratos',
-                        'mimeType': 'application/vnd.google-apps.folder',
-                        'parents': [arquivo['id']]
-                    }
-                    folder = service.files().create(body=folder_metadata, fields='id').execute()
-                    print(folder)
-                    
+                if (arquivo['mimeType'] == 'application/vnd.google-apps.folder' and
+                    arquivo['name'].__contains__(f"faturas_human_{ano}_{mes}")):
+                    pastas_faturas_human = service.files().list(q=f"'{arquivo['id']}' in parents", fields="nextPageToken, files(id, name, mimeType)").execute()
+                    pastas = pastas_faturas_human.get('files', [])
+                    possui_pasta = False
+                    pasta_novos_extratos = None
+                    for pasta in pastas:
+                        if(pasta['mimeType'] == 'application/vnd.google-apps.folder' and 
+                           pasta['name'] == 'novos_extratos'):
+                            possui_pasta = True
+                            pasta_novos_extratos = pasta['id']
+                            break
+                    if not possui_pasta:
+                        print("Pasta novos_extratos não existe. Criando pasta novos_extratos.")
+                        folder_metadata = {
+                            'name': 'novos_extratos',
+                            'mimeType': 'application/vnd.google-apps.folder',
+                            'parents': [arquivo['id']]
+                        }
+                        response_folder = service.files().create(body=folder_metadata, fields='id').execute()
+                        pasta_novos_extratos = response_folder.get('id')
+          
+                    for file, mime_type in zip(files, mime_types):
+                        file_metadata = {
+                            'name': file.name,
+                            'parents': [pasta_novos_extratos],
+                        }
+                        print(file_metadata)
+                        # Cria um arquivo temporário se necessário
+                        if hasattr(file, 'temporary_file_path'):
+                            file_path = file.temporary_file_path()
+                        else:
+                            with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                                temp_file.write(file.read())
+                                file_path = temp_file.name
+
+                        media = MediaFileUpload(file_path, mimetype=mime_type)
+                        service.files().create(body=file_metadata, media_body=media, fields='id').execute()   
             return Response("Arquivos enviados com sucesso.", status=status.HTTP_200_OK)
         except Exception as error:
             print(error)
