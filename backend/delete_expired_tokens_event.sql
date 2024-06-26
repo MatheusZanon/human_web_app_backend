e217f4ed0e615a43a3e06f509053b5ca812dfd4d
MatheusZanon <mzblannes@outlook.com>
2024-06-26 16:56:21 -0300

    fix: removendo arquivos extras

--- backend/delete_expired_tokens_event.sql
+++
@@ -1,4 +0,0 @@
-CREATE EVENT delete_expired_tokens_event
-ON SCHEDULE EVERY 1 hour
-DO
-CALL delete_expired_tokens()
--- backend/delete_expired_tokens_procedure.sql
+++
@@ -1,8 +0,0 @@
-DELIMITER //
-
-CREATE PROCEDURE delete_expired_tokens()
-BEGIN
-	DELETE FROM password_reset WHERE expires_in < NOW();
-END //
-
-DELIMITER ;
